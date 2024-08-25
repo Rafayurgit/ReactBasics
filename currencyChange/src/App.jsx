0781dcc375eddbc47744f648715a0c5c98a30907
farruop <rafayKhanpvt@gmail.com>
2024-08-25 21:41:07 +0530

    imported inputBox component to app and made some changes

--- currencyChange/src/App.jsx
+++ currencyChange/src/App.jsx
@@ -1,9 +1,16 @@
 import { useState } from 'react'
 import InputBox from './components/InputBox'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState(0)
+  const [from, setfrom]= useState("usd");
+  const [to, setTo]= useState("inr");
+  const [convertedAmount, setConvertedAmount]= useState(0);
+
+  const currencyInfo= useCurrencyInfo(from);
+  const opttions= Object.keys(currencyInfo); 
 
   return (
     <>
