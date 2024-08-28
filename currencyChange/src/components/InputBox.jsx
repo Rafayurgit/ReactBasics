0068cb111feb273b504f2da3d62544f29c7cd9fa
farruop <rafayKhanpvt@gmail.com>
2024-08-28 09:03:22 +0530

    made some changes and debug added backgrounf image

--- currencyChange/src/components/InputBox.jsx
+++ currencyChange/src/components/InputBox.jsx
@@ -17,9 +17,9 @@ className=""
 
 
   return (
-    <div className={`felx text-sm bg-white p-3 rounded-xl ${className}`}>
+    <div className={`flex text-sm bg-white p-3 rounded-lg ${className}`}>
       <div className='w-1/2'>
-      <label  className='text-black/40 mb-2 inline-block' htmlFor={amountInputId}>From
+      <label  className='text-black/40 mb-2 inline-block' htmlFor={amountInputId}>
       {label}
       </label>
       <input id={amountInputId} className='outline-none w-full bg-transparent py-1.5'
@@ -32,7 +32,7 @@ className=""
       </div>
 
       <div className='w-1/2 flex flex-wrap justify-end text-right'>
-      <p className='text-black/40 mb-2 w-full'>CurrencyType</p>
+      <p className='text-black/40 mb-2 w-full'>Currency Type</p>
       <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
       value={selectCurrency}
       onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
@@ -45,9 +45,7 @@ className=""
         ))}
         
         
-        <option value="">
-            usd
-        </option>
+        
       </select>
       </div>
     </div>
