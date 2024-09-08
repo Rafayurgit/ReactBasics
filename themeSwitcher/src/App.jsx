1569f0c49b53e31f6754f065e1e9302a2743ba3d
farruop <rafayKhanpvt@gmail.com>
2024-09-08 18:47:16 +0530

    implemented theme mode using context api

--- themeSwitcher/src/App.jsx
+++ themeSwitcher/src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { ThemeProvider } from "./Contexts/Theme";
+import ThemeButton from "./Components/ThemeButton";
+import Card from "./Components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [themeMode, setThemeMode]= useState("light")
+
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector("html").classList.remove("dark" ,"light")
+    document.querySelector("html").classList.add(themeMode)
+  },[themeMode])
+  
 
   return (
-    <>
-      <h1 className="p-5 bg-gray-500 text-xl">Theam Swicher</h1>
-    </>
-  )
+    <ThemeProvider value={{themeMode, lightTheme, darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeButton/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
