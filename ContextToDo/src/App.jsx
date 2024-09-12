17a2d656a75312ffa2dcc328c062a8edce588018
farruop <rafayKhanpvt@gmail.com>
2024-09-12 07:31:32 +0530

    imported components to app.jsx

--- ContextToDo/src/App.jsx
+++ ContextToDo/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { TodoProvider } from "./Contexts";
+import TodoForm from "./components/TodoFrom";
+import { TodoItem } from "./components";
 
 function App() {
   const [todos, setTodos]= useState([])
@@ -41,9 +43,17 @@ function App() {
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
-          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="mb-4">
+            {/* Todo form goes here */}
+            <TodoForm/>
+          </div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
+            {todos.map((todo)=>{
+              <div className="w-full" key={todo.id}>
+                <TodoItem/>
+              </div>
+            })}
           </div>
         </div>
       </div>
