652876866ccf01871e19f17c709d1d03050c4fb0
farruop <rafayKhanpvt@gmail.com>
2024-09-14 09:10:59 +0530

    created basic slice and store

---
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -0,0 +1,17 @@
+import react from 'react'
+
+
+function AddTodo(){
+
+    return(
+        <>
+        <form action="">
+            <input type="text" placeholder='Enter a todo' />
+
+            <button>Add Todo</button>
+        </form>
+        </>
+    )
+}
+
+export default AddTodo;
