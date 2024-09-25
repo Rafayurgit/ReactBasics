99d91937fe2423636b694390dd66adf823489322
farruop <rafayKhanpvt@gmail.com>
2024-09-25 23:25:31 +0530

    created some new components like LogOutBtn,Logo and container

---
+++ AarBlog/src/Components/Header/LogOutBtn.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {useDispatch} from "react-redux"
+import {logIn, logOut} from "../../store/authSlice"
+import  {authService} from "../../appWrite/auth"
+
+function LogOutBtn(){
+
+    const dispatch= useDispatch();
+    const handelLogout=()=>{
+        authService.logOut.then(()=>{
+            dispatch(logOut())
+        })
+    }
+
+    return(
+        <button onClick={handelLogout} className="inline-block px-6 py-2 duration-200 hover:border-y-blue-300 rounded-full">
+            LogOut
+        </button>
+    )
+}
+
+export default LogOutBtn;
---
+++ AarBlog/src/Components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width='100%'}) {
+  return (
+    <div>
+      logo
+    </div>
+  )
+}
+
+export default Logo
