8b5ab447298f5b5e8ee2977d38d3e6b6a2764a8d
farruop <rafayKhanpvt@gmail.com>
2024-10-03 22:15:07 +0530

    creating seperate pages Folder and created SinUpPage

--- AarBlog/src/Components/index.js
+++ AarBlog/src/Components/index.js
@@ -2,12 +2,15 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import LogOutBtn from "./Header/LogOutBtn"; 
+import PostForm from "./post-form/PostForm";
+import Signup from "./Signup";
+import Login from "./Login"
 import Input from "./Input"
 import Logo from "./Logo"
 import Button from './Button'
 import Select from './Select'
 import RTE from "./RTE";
 
-export {Header, Footer, Logo, Container, LogOutBtn, Input , Button, Select, RTE}
+export {Header, Footer, Logo, Container, LogOutBtn, Input , Button, Select, RTE, Signup, Login, PostForm}
 
 
