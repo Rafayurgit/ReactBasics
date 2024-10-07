ee12457408c6b6ee03d315f0bfa77dee3196073e
farruop <rafayKhanpvt@gmail.com>
2024-10-07 23:56:18 +0530

    debug some error and added some functionality to the jokeSuno

--- joke-suno/src/App.js
+++ joke-suno/src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import {usestate, useEffect, useState} from 'react'
 import './App.css';
 
 function App() {
+  const [joke, setJoke]= useState('');
+  const [isLoading, setIsLoading]= useState(false);
+  const [error, setError]= useState(null);
+
+
+
+
+
+  const handelSubmit= async ()=>{
+    //https://api.freeapi.app/api/v1/public/randomjokes
+    setIsLoading(true);
+    setError(null);
+
+    try{
+      const result = await fetch('https://api.freeapi.app/api/v1/public/randomjokes').then(result=> result.json());
+      
+      if(result.success && result.data && result.data.data){
+        const jokesArray= result.data.data;
+        const randomIndex = Math.floor(Math.random()* jokesArray.length )
+        const jokeContent= jokesArray[randomIndex].content;
+       // const newJokes= 
+        
+        setJoke(jokeContent);
+      }else{
+        throw new Error ("Failed to fetch joke");
+      }
+
+    }catch(err){
+      setError(err.message)
+    }finally{
+      setIsLoading(false);
+    }
+
+  }
+
+  // useEffect(  ()=>{
+
+    
+    
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+    <div className='container'>
+      {
+      isLoading ? (<p>loading...</p> )
+      : error ? (<p>{error} </p>)
+      :(<span>{joke}</span>)
+      }
     </div>
+
+    <div className='button-container'>
+      <button onClick={handelSubmit} disabled={isLoading}>Tell me a joke</button>
+    </div>
+   </div>
   );
 }
 
