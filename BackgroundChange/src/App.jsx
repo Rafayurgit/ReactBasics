c73a8cca1f8546327c79922b85e934fc65f15f35
farruop <rafayKhanpvt@gmail.com>
2025-03-10 01:43:36 +0530

    Add: change backGroundColor dyanmically now

--- BackgroundChange/src/App.jsx
+++ BackgroundChange/src/App.jsx
@@ -3,8 +3,9 @@ import { useState } from "react"
 function App() {
   const [color, setColor]= useState("black");
 
-  const handelColor=(color)=>{
+  const handelColor=(event)=>{
     console.log("button clicked");
+    const color= event.target.style.backgroundColor;
     setColor(color)
   }
 
@@ -15,10 +16,10 @@ function App() {
         <div className="fixed flex justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl ">
         <button className="px-4 py-2 outline-none shadow-lg text-white rounded-full" style={{backgroundColor: "red" }} onClick={ ()=>setColor("red") }  >Red</button>
         <button className=" rounded-full px-4 py-2 shadow-lg text-white outline-none" style={{backgroundColor:"green"}} onClick={ ()=> {console.log("Button Clicked") ,setColor("green")} } >Green</button>
-        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "blue"}} onClick={()=> handelColor("blue")}  > Blue</button> 
-        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "purple"}} onClick={()=> handelColor("purple")}> purple</button>
-        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "black"}}  onClick={()=>{handelColor("black")}}> Black</button>
-        <button className="rounded-full px-4 py2 outline-none shadow-lg text-black " style={{backgroundColor: "yellow"}} > yellow</button>
+        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "blue"}} onClick={ handelColor }  > Blue</button> 
+        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "purple"}} onClick={handelColor}> purple</button>
+        <button className="rounded-full px-4 py2 outline-none shadow-lg text-white " style={{backgroundColor: "black"}}  onClick={handelColor}> Black</button>
+        <button className="rounded-full px-4 py2 outline-none shadow-lg text-black " style={{backgroundColor: "yellow"}} onClick={handelColor}> yellow</button>
         </div>
       </div>
     </div>
