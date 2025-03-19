c5858fa3440774e6d18e85ccf1ddc267637ce4e5
farruop <rafayKhanpvt@gmail.com>
2025-03-19 23:51:06 +0530

    Update useCurrencyHook

--- currencyChange/src/hooks/useCurrencyInfo.js
+++ currencyChange/src/hooks/useCurrencyInfo.js
@@ -1,15 +1,19 @@
 import { useState, useEffect } from "react";
+//fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
 
-function useCurrencyInfo(currency){
+
+
+function useCurrency(currency){
+
     const [data, setData]= useState({});
 
     useEffect(()=>{
-        //fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
+        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res)=>res.json())
-        .then((res)=>setData(res[currency]))
-    },[currency])    
-    return data
+        .then((res)=> setData(res[currency]))
+    }, [currency])
+
+    return data;
 }
 
-export default useCurrencyInfo;
+export default useCurrency;
