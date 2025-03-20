87a6fb3326045dbfa00de8d13f1860d727fbc15a
farruop <rafayKhanpvt@gmail.com>
2025-03-20 23:17:09 +0530

    update input box for amount

--- currencyChange/src/components/InputBox.jsx
+++ currencyChange/src/components/InputBox.jsx
@@ -1,55 +1,53 @@
-import React from 'react'
-import { useId } from 'react'
+import {useState} from "react";
+
 
 function InputBox({
-label,
-amount,
-selectCurrency="usd",
-onAmountChange,
-onCurrencyChange,
-amountDisable=false,
-currencyDisable=false,
-currencyOptions=[],
-className=""
+  label,
+  className,
+  amount,
+  onAmountChange,
+  disableAmount,
+
+  currencyOptions=[],
+  selectCurrenct="usd",
+  onCurrecnyChange,
+  disableCurrency,
+  
+  className = "",
 }) {
-
-    const amountInputId=useId();
-
+ 
 
   return (
-    <div className={`flex text-sm bg-white p-3 rounded-lg ${className}`}>
-      <div className='w-1/2'>
-      <label  className='text-black/40 mb-2 inline-block' htmlFor={amountInputId}>
-      {label}
-      </label>
-      <input id={amountInputId} className='outline-none w-full bg-transparent py-1.5'
-      type='number'
-      placeholder='Amount'
-      value={amount}
-      disabled={amountDisable}
-      onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
-      />
+      <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+          <div className="w-1/2">
+              <label  className="text-black/40 mb-2 inline-block">
+                  {label}
+              </label>
+              <input
+                  
+                  className="outline-none w-full bg-transparent py-1.5"
+                  type="number"
+                  placeholder="Amount"
+                  disabled={disableAmount}
+                  onChange={(e)=> onAmountChange && onAmountChange(Number(e.target.value))}
+                  value={amount}
+              />
+          </div>
+          <div className="w-1/2 flex flex-wrap justify-end text-right">
+              <p className="text-black/40 mb-2 w-full">Currency Type</p>
+              <select
+                  className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                  
+              >
+                  
+                      <option value="usd">
+                          usd
+                      </option>
+              
+              </select>
+          </div>
       </div>
-
-      <div className='w-1/2 flex flex-wrap justify-end text-right'>
-      <p className='text-black/40 mb-2 w-full'>Currency Type</p>
-      <select className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
-      value={selectCurrency}
-      onChange={(e)=> onCurrencyChange && onCurrencyChange(e.target.value)}
-      disabled={currencyDisable}
-      >
-        {currencyOptions.map((currency)=>(
-            <option key={currency}value={currency}>
-             {currency}   
-            </option>
-        ))}
-        
-        
-        
-      </select>
-      </div>
-    </div>
-  )
+  );
 }
 
-export default InputBox
+export default InputBox;
