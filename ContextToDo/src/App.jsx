e3d9d643da87a989e60f59bbd28d9239da723ba2
farruop <rafayKhanpvt@gmail.com>
2025-04-08 23:28:24 +0530

    Add: addTodo functionality

--- ContextToDo/src/App.jsx
+++ ContextToDo/src/App.jsx
@@ -3,27 +3,17 @@ import "./App.css";
 import { TodoProvider } from "./Contexts/Todocontext";
 import TodoForm from "./components/TodoFrom";
 import  TodoItem from "./components/TodoItem";
+import { data } from "autoprefixer";
 
 function App() {
   const [todos, setTodos]= useState([])
 
   const addTodo=(todo)=>{
-    setTodos((prev)=>[{id:Date.now(), ...todo}, ...prev])
+    setTodos((preVtodo)=> [{id: Date.now() , ...todo }, ...preVtodo])
   }
 
-  const updateTodo=(id, todo)=>{
-    setTodos((prev)=>prev.map((prevTodo)=>    (prevTodo.id===id ? todo:prevTodo)))
-    
-
-  }
-
-  const deleteTodo=(id)=>{
-    setTodos((prev)=>prev.filter((todo)=> todo.id !==id))
-    
-  }
-
-  const toggleComplete=(id)=>{
-    setTodos((prev)=> prev.map((prevTodo)=>prevTodo.id===id ? {...prevTodo, completed: !prevTodo.completed}: prevTodo))
+  const deleteTodo=()=>{
+    setTodos()
   }
 
   useEffect(()=>{
@@ -40,7 +30,7 @@ function App() {
   
 
   return (
-    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+    <TodoProvider value={{todos, addTodo, deleteTodo, updateTodo,toggleTodo}}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
