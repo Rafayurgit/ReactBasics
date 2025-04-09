e3af3cffcffcca9338f19364ce48bdf1ca88baf5
farruop <rafayKhanpvt@gmail.com>
2025-04-09 23:38:49 +0530

    Add: update todoForm

--- ContextToDo/src/components/TodoFrom.jsx
+++ ContextToDo/src/components/TodoFrom.jsx
@@ -4,25 +4,26 @@ import { useTodo } from '../Contexts';
 
 function TodoForm() {
 
-    const [todo, setTodo]= useState("");
-    const {addTodo}=useTodo()
+    const [todoMsg, setTodoMsg] = useState([])
+    const {addTodo} = useTodo()
 
-    const add =(e)=>{
-        e.preventDefault()
+    handelSubmit=()=>{
+        e.preventDefault();
 
         if(!todo) return
-        addTodo({todo, completed:false})
-        setTodo("")
+        addTodo({todoMsg, complete: false})
+        setTodoMsg("")
+
     }
 
     return (
-        <form  className="flex" onSubmit={add}>
+        <form  className="flex" onSubmit={handelSubmit}>
             <input 
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
                 value={todo}
-                onChange={(e)=>setTodo(e.target.value)}
+                onChange={(e)=> setTodoMsg(e.target.value)}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
